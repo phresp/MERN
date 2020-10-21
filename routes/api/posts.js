2812f6f521423f789c44101ff30d7a0a6b8780ff
Philipp Spanner <philipp.spanner@googlemail.com>
2020-10-21 14:57:44 +0200

    Register and Login working

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // @route   GET /api/posts/test
-// @desc    Tess posts route
+// @desc    Test posts route
 // @access  Public
 
 router.get('/test', (req, res) => res.json({msg: "Posts Works!"}));
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // @route   GET /api/profile/test
-// @desc    Tess profile route
+// @desc    Test profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works!"}));
 
