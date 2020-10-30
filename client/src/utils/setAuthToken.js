9185851457fd444e91ad888b2ce4f7939ec78282
Philipp Spanner <philipp.spanner@googlemail.com>
2020-10-30 14:35:41 +0100

    Login/Logout redirect if logged in

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -5,7 +5,7 @@ const setAuthToken = (token) => {
     //Apply to every request
     axios.defaults.headers.common["Authorization"] = token;
   } else {
-    //Delete huth header
+    //Delete auth header
     delete axios.defaults.headers.common["Authorization"];
   }
 };
